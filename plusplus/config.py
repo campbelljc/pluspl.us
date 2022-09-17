05195e0d666b1668d2c94dc04c08b47d238c8864
campbelljc <jonathan@campbelljc.com>
2022-09-17 19:44:07 -0400

    test slack revert

--- plusplus/config.py
+++ plusplus/config.py
@@ -5,8 +5,8 @@ NAME = os.environ.get("NAME")
 SLACK_CLIENT_ID = os.environ.get('SLACK_CLIENT_ID')
 SLACK_CLIENT_SECRET = os.environ.get('SLACK_CLIENT_SECRET')
 SLACK_SCOPES = "chat:write,im:history,im:write,mpim:history,mpim:write,team:read,channels:history,app_mentions:read"
-#SLACK_OAUTH_URL = f"https://slack.com/oauth/v2/authorize?client_id={SLACK_CLIENT_ID}&scope={SLACK_SCOPES}"
-SLACK_OAUTH_URL = f"https://slack.com/openid/connect/authorize?client_id={SLACK_CLIENT_ID}&scope={SLACK_SCOPES}"
+SLACK_OAUTH_URL = f"https://slack.com/oauth/v2/authorize?client_id={SLACK_CLIENT_ID}&scope={SLACK_SCOPES}"
+#SLACK_OAUTH_URL = f"https://slack.com/openid/connect/authorize?client_id={SLACK_CLIENT_ID}&scope={SLACK_SCOPES}"
 SLACK_SIGNING_SECRET = os.environ.get('SLACK_SIGNING_SECRET')
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
